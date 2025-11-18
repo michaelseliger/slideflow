fff575caf377d14a4da78b6389176dadbe77dae5
Michael Seliger <development@michaelseliger.com>
2025-11-18 13:44:04 +0100

    Initial Commit

---
+++ slideflow/slideflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  slideflowApp.swift
+//  slideflow
+//
+//  Created by Michael Seliger on 18.11.25.
+//
+
+import SwiftUI
+
+@main
+struct slideflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
